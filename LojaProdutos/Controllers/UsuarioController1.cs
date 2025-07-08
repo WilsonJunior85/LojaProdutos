d01a5138232c33738b864e775331a6e4f621652d
WILSON JUNIOR <wilson.pereira@serede.com.br>
2025-07-08 14:37:33 -0300

    Iniciando Criação de Modelo de Usuário

---
+++ LojaProdutos/Controllers/UsuarioController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LojaProdutos.Controllers
+{
+    public class UsuarioController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- LojaProdutos/Program.cs
+++ LojaProdutos/Program.cs
@@ -2,6 +2,7 @@ using LojaProdutos.Data;
 using LojaProdutos.Services.Categoria;
 using LojaProdutos.Services.Estoque;
 using LojaProdutos.Services.Produto;
+using LojaProdutos.Services.Usuario;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -20,6 +21,7 @@ builder.Services.AddDbContext<DataContext>(options =>
 builder.Services.AddScoped<IProdutoInterface, ProdutoService>();
 builder.Services.AddScoped<ICategoriaInterface, CategoriaService>();
 builder.Services.AddScoped<IEstoqueInterface, EstoqueService>();
+builder.Services.AddScoped<IUsuarioInterface, UsuarioService>();
 
 
 
